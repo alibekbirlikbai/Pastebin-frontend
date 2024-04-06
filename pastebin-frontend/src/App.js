e2253a0b86809862af004261de1d4b118b39df5d
Alibek Birlikbai <87764579+alibekbirlikbai@users.noreply.github.com>
2024-04-07 03:21:35 +0500

    test connect with API

--- pastebin-frontend/src/App.js
+++ pastebin-frontend/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/api/v1/pastbin/bins');
+      const jsonData = await response.json();
+      console.log('Received data:', jsonData); // Log received data
+      setData(jsonData);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Data from API</h1>
+      <ul>
+        {data.map(bin => (
+          <li key={bin.id}>{bin.url}</li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ pastebin-frontend/src/services/PastebinService.js
@@ -0,0 +1,20 @@
+// import axios from 'axios'; 
+
+// const PASTEBIN_API_BASE_URL = "http://localhost:8080/api/v1/pastbin/bins";
+
+// class PastebinService {
+
+//     getBins(){
+//         return axios.get(PASTEBIN_API_BASE_URL);
+//     }
+
+//     createBin(bin){
+//         return axios.post(PASTEBIN_API_BASE_URL, bin);
+//     }
+
+//     getBinByHash(hashOfBin){
+//         return axios.get(PASTEBIN_API_BASE_URL + '/' + hashOfBin);
+//     }
+// }
+
+// export default new PastebinService()
